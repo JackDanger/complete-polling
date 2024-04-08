b40c6eb62ffec636c4adb200be01fd361e1a8e0f
Jack Danger <github@jackcanty.com>
2024-04-08 16:54:54 -0700

    disabling option size validation

--- server/app/models/poll.rb
+++ server/app/models/poll.rb
@@ -2,7 +2,7 @@ class Poll < ApplicationRecord
   has_many :options, -> { order(index: :asc) }, dependent: :destroy, class_name: 'PollOption'
 
   validates :title, presence: true
-  validate :must_have_four_poll_options
+  #validate :must_have_four_poll_options
 
   def must_have_four_poll_options
     unless options.size == 4
