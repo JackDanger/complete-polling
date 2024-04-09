36016f65e9c4fcaf33268101306f1cca372bcca4
Jack Danger <github@jackcanty.com>
2024-04-08 17:33:39 -0700

    Serializing options

--- server/app/controllers/polls_controller.rb
+++ server/app/controllers/polls_controller.rb
@@ -1,23 +1,23 @@
-class PollsController < ApplicationController
+# frozen_string_literal: true
 
+class PollsController < ApplicationController
   def index
-    render json: Poll.all.includes(:options)
+    render json: Poll.all.joins(:options)
   end
 
   def create
-    record = Poll.create! params[:poll].permit(:title, :description, :options)
+    record = Poll.create! params[:poll].permit(:title, :description, options_attributes: :text)
     render json: record
   end
 
   def show
-    record = Poll.includes(:options).find(params[:id])
+    record = Poll.joins(:options).find(params[:id])
     render json: record
   end
 
   def update
     record = Poll.find(params[:id])
-    record.update!(params[:poll].permit(:title, :description, :options))
+    record.update!(params[:poll].permit(:title, :description, options_attributes: :text))
     render json: record
   end
-
 end
