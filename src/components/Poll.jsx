c8e604932a12b8a0bd2bfa998566cde86ae63a89
Jack Danger <github@jackcanty.com>
2024-04-08 19:25:14 -0700

    Starting to style things

--- src/components/Poll.jsx
+++ src/components/Poll.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { withRouter } from '../common/with-router.jsx';
 import PollDataService from "../services/poll.service.jsx";
 import { Link } from 'react-router-dom';
@@ -6,128 +6,38 @@ import Option from "./Option";
 
 const Poll = (props) => {
 
-  const [state, setState] = useState(props);
+  const [pollId, setPollId] = useState();
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [options, setOptions] = useState([]);
 
-  function onChangeTitle(e) {
-    const title = e.target.value;
-
-    setState({
-      ...state,
-      title: title
-    });
-  }
-
-  function onChangeDescription(e) {
-    const description = e.target.value;
-
-    setState({
-      ...state,
-      description: description,
-    });
-  };
-  function onChangeOption(index) {
-    return (optionData) => {
-      state.options[index] = optionData;
+  useEffect(() => {
+    if (pollId) {
+      PollDataService.get(pollId).then((response) => {
+        setTitle(response.data.title || '');
+        setDescription(response.data.description || '');
+        setOptions(response.data.options_attributes || []);
+      });
     }
-  }
+  }, [pollId]);
 
-  function getPoll(id) {
-    PollDataService.get(id)
-      .then(response => {
-        setState({ ...response.data })
-        console.log(response.data);
-      })
-      .catch(e => {
-        console.log(e);
-      });
-  }
-
-  function updatePoll() {
-    PollDataService.update(state.id, state)
-      .then(response => {
-        console.log(response.data);
-        setState({
-          ...state,
-          message: "The poll was updated successfully!"
-        });
-      })
-      .catch(e => {
-        console.log(e);
-      });
-  }
-
-  function deletePoll() {
-    PollDataService.delete(state.id)
-      .then(response => {
-        console.log(response.data);
-        props.router.navigate('/polls');
-      })
-      .catch(e => {
-        console.log(e);
-      });
-  }
-
-  if (!state.id && props.router.params.id) {
-    getPoll(props.router.params.id);
+  if (!pollId && props.router.params.id) {
+    setPollId(props.router.params.id);
   }
 
   return (
-    props.edit ? (
-      <div className="edit-form">
-        <h4>Poll</h4>
-        <form>
-          <div className="form-group">
-            <label htmlFor="title">Title</label>
-            <input
-              type="text"
-              className="form-control"
-              id="title"
-              value={state.title}
-              onChange={onChangeTitle}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="description">Description</label>
-            <input
-              type="text"
-              className="form-control"
-              id="description"
-              value={state.description}
-              onChange={onChangeDescription}
-            />
-          </div>
-          <div>
-            {(props.options).map((option, index) => (
-              <Option index={index} edit={true} onChange={onChangeOption(index)} text={option.text} />
-            ))}
-          </div>
-        </form>
-        <button
-          className="badge badge-danger mr-2"
-          onClick={deletePoll}
-        >
-          Delete
-        </button>
-
-        <button
-          type="submit"
-          className="badge badge-success"
-          onClick={updatePoll}
-        >
-          Update
-        </button>
-      </div>
-    ) : (
+    pollId && (
       <div>
         <Link to={"/polls"}>Back to Polls</Link>
         <div>
-          <h2>{props.title}</h2>
+          <h2>{title}</h2>
+          <small><Link to={`/polls/${pollId}/edit`}>Edit</Link></small>
         </div>
         <div>
-          {props.description}
+          {description}
         </div>
-        {(props.options).map((props, index) => (
-          <Option pollId={props.id} {...props.options} />
+        {(options).map((option, index) => (
+          <Option id={option.id} text={option.text} index={index} pollId={pollId} />
         ))}
       </div>
     )
