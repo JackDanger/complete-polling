b5d7eb1ddecdc16fd395a12a40d465344f33456d
Jack Danger <github@jackcanty.com>
2024-04-09 09:01:54 -0700

    Using the bash jobs subsystem

--- run.sh
+++ run.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
+set -x
 set -euo pipefail
+
+function cleanup {
+  wait
+  docker ps | grep [j]ackdanger | awk '{print $1}' | xargs docker kill
+}
 
 (
 	cd server
   docker build -t jackdanger:backend .
-  docker run --rm -p 1337:1337 -e RAILS_ENV=development -it jackdanger:backend
+  docker run -p 1337:1337 -e RAILS_ENV=development jackdanger:backend
 ) &
 
 (
-	brew install vite
-	yarn
-	yarn dev
+  sleep 15
+  open http://localhost:5173/polls
 ) &
 
+brew install vite
+yarn
+yarn dev
 
-sleep 15
-open http://localhost:5173/polls
+trap cleanup EXIT
+exit
