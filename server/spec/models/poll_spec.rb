be9ed8c516621c13180953cb1302c014a3f9ffed
Jack Danger <github@jackcanty.com>
2024-04-08 17:44:00 -0700

    removing restriction

--- server/spec/models/poll_spec.rb
+++ server/spec/models/poll_spec.rb
@@ -9,16 +9,6 @@ RSpec.describe Poll do
     let(:poll) { Poll.new(title: 'This is a poll') }
     subject { poll }
 
-    context 'without any options' do
-      before do
-        poll.options.clear
-      end
-
-      it 'is invalid' do
-        expect(subject).to_not be_valid
-      end
-    end
-
     context 'with four options' do
       before do
         4.times { |n| poll.options.build text: "option #{n}", index: n }
